711e4dc4f553dc22dc5c8a378d836b41db65485f
Duy Node <duynode@Duys-MBP.lan>
2023-12-06 00:00:26 +0700

    Setup Tail,Color,Styling Text

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
 
-      <p>Duy Node</p>
+      <Username />
     </header>
   );
 }
