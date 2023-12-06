bf6880f34237d19a68d3df5bf4572ef70c49f4f2
Duy Node <duyle.sales@gmail.com>
2023-12-06 10:47:04 +0700

    Responsive Desgin

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Username() {
-  return <div className="text-sm font-semibold">Duy Node</div>;
+  return <div className="text-sm font-semibold hidden">Duy Node</div>;
 }
