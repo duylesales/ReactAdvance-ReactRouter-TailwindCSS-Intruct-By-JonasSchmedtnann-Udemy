b67bd150c422c9a7e5da42964dd1842c8c2ea1e8
Duy Node <duyle.sales@gmail.com>
2023-12-06 12:32:53 +0700

    Styling Form Elements

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -18,6 +18,7 @@ export default function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="rounded-full px-4 py-2 text-sm bg-yellow-100 placeholder:text-stone-900 w-28 sm:w-64 sm:focus:w-72 transitions-all duration-300 focus:ring focus:ring-yellow-500 focus:outline-none focus:ring-opacity-50 "
       />
     </form>
   );
