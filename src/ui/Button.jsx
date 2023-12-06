d6a542e7915e8bcaa69b8b7a659007f0bb9cc5e2
Duy Node <duyle.sales@gmail.com>
2023-12-06 17:23:07 +0700

    Styling the Cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -9,7 +9,8 @@ export default function Button({children,disabled,to,type}) {
   const styles={
     
     primary:base + ' py-3 px-4 md:px-6 md:py-4',
-    small:base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs'
+    small:base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:"border-2 border-stone-300 uppercase font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:bg-stone-300 focus:ring-stone-200 focus:ring-offset-2 active:bg-slate-400 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5"
   }
 
     if(to) return (<Link to={to} className={styles[type]}>{children}</Link>)
