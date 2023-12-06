46fe8a49527a0feb52313909ecf4d3428e1e923b
Duy Node <duyle.sales@gmail.com>
2023-12-06 15:03:02 +0700

    Absolute Positioning,z-index and more

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Loader() {
-  return <div className="loader"></div>;
+  return <div className="absolute bg-slate-200/20 inset-0 backdrop-blur-sm flex items-center justify-center ">
+  <div className="loader"></div>
+  </div>
 }
