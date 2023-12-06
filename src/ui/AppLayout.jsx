912b582b3dd5a77ec6b242c03829b08827c6829c
Duy Node <duyle.sales@gmail.com>
2023-12-06 11:34:02 +0700

    Grid

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,14 +9,16 @@ export default function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto _1fr_auto] ">
       {isLoading && <Loader />}
 
       <Header />
 
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className=" max-w-3xl  mx-auto">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
